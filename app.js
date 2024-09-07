8ebb55ca632fc3fca22966181c6b6421700a2a4e
AdnanShahidBakkar <adnanshahidbakkar@gmail.com>
2024-09-07 11:03:20 +0500

    Ch 1 to 4

---
+++ app.js
@@ -0,0 +1,86 @@
+// Chapter 01 (Alerts)
+
+// q 01
+// a)
+alert(" First Name --- Adnan")
+// b)
+alert(" Last Name --- Shahid")
+// c)
+alert(" Email --- adnanshhaidbakkar@gmail.com")
+// d)
+alert("Phone Number --- 03242702170")
+
+// q 02
+alert("You're learning javaScript!")
+
+// q 03
+alert("Hi, l am a FrontEnd Developer")
+
+console.log("Chater 02 Start");
+
+alert("Class Task Start ------------------------")
+
+
+// Chapter 02 Variable for String
+
+// q 01
+var myName
+
+// q 02
+var myIntro
+var myIntro = "Alkhamdulillah I am aMuslim";
+console.log(myIntro);
+
+
+// q 02
+var teamName = "pakDeveloper"
+console.log(teamName)
+
+
+var bestMan = "Charlie"
+console.log(bestMan)
+
+console.log("Chater 03 Start");
+
+
+// Variables for Numbers
+
+// q 01
+var caseQty;
+// q 02
+caseQty = 144;
+console.log(caseQty)
+
+// q 03
+var num = 9;
+console.log(num);
+
+// q 04
+var sum = 10;
+var sum2 = 20;
+
+console.log(sum + sum2);
+
+// q 05
+var orderTotal;
+
+var grocery = 700;
+var vegeTables = 350;
+
+var orderTotal = grocery + vegeTables;
+
+console.log(orderTotal);
+
+// q 06
+var state = 13;
+var state = state + 13;
+
+console.log(state);
+
+
+
+
+
+
+
+
---
+++ task.js
@@ -0,0 +1,116 @@
+
+// //Questions
+
+// Alerts
+// q 01
+alert("Error!Please Enter a Vali Password")
+
+// q 02
+alert("Welcome Js \n Happy Coding")
+
+// q 03
+alert("Welcome Js")
+
+// q 03
+alert("Happy Coding")
+
+
+
+alert("Chapert 02 Start")
+// Variable for Strings
+
+// q 01
+var username;
+console.log(username)
+
+// q 02
+var myName = "adnan";
+console.log(myName);
+
+// q 03
+var message;
+message = "Hello Word";
+alert(message)
+
+// q 04
+var jon = "John doe"
+alert(jon)
+
+var old = "15 years old"
+alert(old)
+
+var certified = "Certified Mobile Appliction"
+alert(certified)
+
+// q 05
+
+var pizza = "pizza \n pizz \n piz \n pi \n p"
+alert(pizza)
+
+// q 06
+var email = "adnanshahidxyz@gmail.com"
+alert(email)
+
+// q 07
+var book = "A smarter way to \n learn Javascript"
+alert(book)
+
+// q 08
+var end = "----------XXXXX------------"
+alert(end)
+
+
+
+alert("Chapert 03 Start")
+// Variables for Numbers
+
+// q 01
+var age = 20;
+alert("I am " + age +" "+ "years old")
+
+// q 02
+var site = 14;
+alert("You have Visited this Site " + site + " times")
+
+// q 03
+
+document.write("Class Task Chapter Variable For Numbers"+ "<br> <br> <br>")
+
+var year = 2004; 
+document.write("Question 03 ==> My birth year"+ " " + year + "<br>"+ "Data type of declared variable is number" + "<br>")
+
+// q 04
+document.write(" Question 04 ==> www.clothing store.com " + "<br> <br> <br> ")
+
+var $name = "john doe";
+var product = "Clothing Store";
+var order = "5 t-Shirt(s)";
+
+document.write($name + " " + order + " " + product)  
+
+
+
+alert("Chapert 04 Start")
+// legal and illegal
+
+// q 01
+// var a = 15, b = 20, c = 25;
+
+// q 02
+// var aa = 10;
+// var ab = 20;
+// var ac = 30;
+// var ad = 40;
+// var ae = 50;
+
+// q 03
+var head = "Rules for naming Js Variables-- <br> <br>"
+document.write(head)
+
+document.write("a) Variables name can only contain -- $my13house , my_home23 or java$cript <br> <br>")
+
+document.write("b) Variables names case sensetive -- adnan , name ,html , css , js <br> <br>")
+
+document.write("c) Variables name should not be js keywords -- adnan-shahid , Geeks-for-geeks , 124welcome <br>")
+
+
